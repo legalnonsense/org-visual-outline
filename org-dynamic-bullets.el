e45c64667f06e1738e5f89b2346e5c16c729b45e
Jeff Filipovits <jrfilipovits@gmail.com>
2021-03-10 12:45:07 -0500

    add before first heading predicate

--- org-dynamic-bullets.el
+++ org-dynamic-bullets.el
@@ -268,16 +268,17 @@ the heading and before the next heading."
 All fontifying functions use this function as their base.  
 This function searches the region for the headline regexp and calls 
 `org-dynamic-bullets-refresh-func' to act on the matches."
-  (org-with-wide-buffer 
-   (save-excursion
-     (save-match-data 
-       (goto-char beg)
-       (while
-	   (re-search-forward org-dynamic-bullets--heading-re end t)
-	 (save-excursion
-	   (funcall org-dynamic-bullets-refresh-func
-		    (match-beginning 1)
-		    (match-end 1))))))))
+  (unless (org-before-first-heading-p)
+    (org-with-wide-buffer 
+     (save-excursion
+       (save-match-data 
+	 (goto-char beg)
+	 (while
+	     (re-search-forward org-dynamic-bullets--heading-re end t)
+	   (save-excursion
+	     (funcall org-dynamic-bullets-refresh-func
+		      (match-beginning 1)
+		      (match-end 1)))))))))
 
 (defun org-dynamic-bullets--fontify-buffer (&rest _)
   "Fontify the entire buffer."
